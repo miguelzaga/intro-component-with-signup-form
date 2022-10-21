e860e4cf89f5a0da780d66d0ded866196497e03e
Miguel <miguezaga2@hotmail.com>
2022-10-21 11:04:28 -0500

    setup files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1>New App</h1>
+      Learn to code by watching others See how experienced developers solve
+      problems in real-time. Watching scripted tutorials is great, but
+      understanding how developers think is invaluable. Try it free 7 days then
+      $20/mo. thereafter First Name Last Name Email Address Password Claim your
+      free trial By clicking the button, you are agreeing to our Terms and
+      Services
     </div>
   );
 }
