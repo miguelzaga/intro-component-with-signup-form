705a6ec862ccaf2293c09d3c309a8dfc4d44adda
Miguel <miguezaga2@hotmail.com>
2022-10-21 12:21:26 -0500

    add html

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div>
-      Learn to code by watching others See how experienced developers solve
-      problems in real-time. Watching scripted tutorials is great, but
-      understanding how developers think is invaluable. Try it free 7 days then
-      $20/mo. thereafter First Name Last Name Email Address Password Claim your
-      free trial By clicking the button, you are agreeing to our Terms and
-      Services
-    </div>
+    <main>
+      <div>
+        <h1> Learn to code by watching others </h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+      </div>
+      <div>
+        <div>Try it free 7 days then $20/mo. thereafter</div>
+        <form>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name " />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Password" />
+          <button type="submit">Claim your free trial</button>
+          <p>
+            By clicking the button, you are agreeing to our Terms and Services
+          </p>
+        </form>
+      </div>
+    </main>
   );
 }
 
